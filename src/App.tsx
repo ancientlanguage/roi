0589bc732cbd2f79c00ba4ee7a0ad9004477f678
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2020-08-23 07:12:10 -0700

    Sketch values.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import './App.css';
 
+// You have a bag with 'size' items inside.
+interface Size {
+  size: number;
+}
+
+// If you were to pick an item from the bag,
+// this represents which item it is.
+// Without a size, we don't know how big it could be.
+interface UnsizedValue {
+  value: number;
+}
+
+// We have a value that is bounded by size.
+// 'value' should be less than 'size' and
+// greater than or equal to 0.
+// 0 <= value < size
+interface Value extends Size, UnsizedValue {
+}
+
+const isValidValue: (value : Value) => boolean =
+  (value) =>
+    value.value >= 0 && value.value <= value.size;
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
         <p>
           Shark
         </p>
-      </header>
+      </div>
+      <div>
+        <canvas />
+      </div>
     </div>
   );
 }
